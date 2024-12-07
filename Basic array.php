0cabf9ce85a6bd1ec538ea6b55fca68c9727f6c6
Ragav48 <nragavendran362@gmail.com>
2024-12-07 11:11:58 +0530

    day 3

---
+++ Basic array.php
@@ -0,0 +1,25 @@
+Array Programs:
+
+<!-- To display an array in output: -->
+
+<?php
+
+$players = ['Raina', 'ABD', 'Jinx'];
+
+foreach ($players as $player) {
+    echo $player . "<br>";
+}
+
+?>
+
+
+<!-- 2nd one -->
+
+<?php
+
+$arr = ["1", "22", "33"];
+print_r($arr);
+
+?>
+
+<!-- // Insert a new element in the array -->
