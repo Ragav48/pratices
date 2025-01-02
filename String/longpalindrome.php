c4915e89c6a99748e69790ff8f08ff9b2f6c5a0b
Ragav48 <nragavendran362@gmail.com>
2025-01-02 18:22:29 +0530

    update the next ones

---
+++ String/longpalindrome.php
@@ -0,0 +1,30 @@
+<?php
+
+function longestPalindromicSubstring($str)
+{
+    $n = strlen($str);
+    $start = 0;
+    $maxLength = 1;
+
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = $i; $j < $n; $j++) {
+            $isPalindrome = true;
+
+            for ($k = 0; $k < ($j - $i + 1) / 2; $k++) {
+                if ($str[$i + $k] !== $str[$j - $k]) {
+                    $isPalindrome = false;
+                    break;
+                }
+            }
+
+            if ($isPalindrome && $j - $i + 1 > $maxLength) {
+                $start = $i;
+                $maxLength = $j - $i + 1;
+            }
+        }
+    }
+
+    return substr($str, $start, $maxLength);
+}
+
+echo longestPalindromicSubstring("babad"); // Output: "bab" or "aba"
